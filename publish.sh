84471957f7fca069d5be224bf8d8c76827f26fd4
Simon Osborne <simonwosborne@gmail.com>
2019-08-26 12:52:53 +0100

    Update circle ci

--- publish.sh
+++ publish.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-
-if [[ $(git status -s) ]]
-then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
-fi
-
 echo "Deleting old publication"
 rm -rf public
 mkdir public
@@ -14,7 +7,7 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-# git worktree add -B gh-pages public upstream/gh-pages
+git worktree add -B gh-pages public upstream/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
@@ -25,6 +18,7 @@ hugo
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"  
 
+git pull --rebase
+git push 
 cd ..
-# git push upstream gh-pages
 echo "Updated gh-pages branch"
