a8946a57e8840ac70a620e56b148ca99e4946daf
Simon Osborne <simonwosborne@gmail.com>
2019-08-27 06:08:09 +0100

    update image

--- publish.sh
+++ publish.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 
-echo "Deleting old publication"
-rm -rf public
-mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
-
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
-
-echo "Removing existing files"
-rm -rf public/*
-
 echo "Generating site"
-hugo
+hugo -d docs
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"  
+cd git add --all && git commit -m "Publishing to gh-pages (publish.sh)"  
 
-git pull --rebase
 git push 
-cd ..
 echo "Updated gh-pages branch"
